74d02cf86ab797b8f9370a4dd75e138eccfe9a49
timerainbow <timerainbow@163.com>
2019-06-10 16:24:05 +0800

    step3

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/',function(req,res){
 	//res.send('<h1>Hello lianghui</h1>');
 	res.sendFile(__dirname + '/index.html');
 });
+
+io.on('connection',function(socket){
+	console.log('a user connected');
+})
 
 http.listen(3000,function(){
 	console.log('listening on port:3000');
