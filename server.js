7716d189febcbb7a9574ff836164717bb96dce57
timerainbow <timerainbow@163.com>
2019-06-10 16:38:02 +0800

    处理消息的发送与接收

--- server.js
+++ server.js
@@ -19,6 +19,11 @@ io.on('connection',function(socket){
       usocket[name] = socket
       io.emit("join", name) //服务器通过广播将新用户发送给全体群聊成员，前端处理
     })
+
+    //new addition
+	socket.on("message", function (msg) {
+	  io.emit("message", msg) //将新消息广播出去，交给前端处理
+	})
 })
 
 http.listen(3000,function(){
