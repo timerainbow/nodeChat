b6b6824e518234b68a3be5081e44768332a444e0
timerainbow <timerainbow@163.com>
2019-06-10 16:31:47 +0800

    添加用户加入聊天天提示功能

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+
+var usocket = []; //全局变量
+
+
 app.get('/',function(req,res){
 	//res.send('<h1>Hello lianghui</h1>');
 	res.sendFile(__dirname + '/index.html');
@@ -9,6 +13,12 @@ app.get('/',function(req,res){
 
 io.on('connection',function(socket){
 	console.log('a user connected');
+
+    //监听join事件
+    socket.on("join", function (name){
+      usocket[name] = socket
+      io.emit("join", name) //服务器通过广播将新用户发送给全体群聊成员，前端处理
+    })
 })
 
 http.listen(3000,function(){
